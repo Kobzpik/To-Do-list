5626cc6d74ce312836975769dbd56feae1954324
Kobzpik <prabath.pik2299@gmail.com>
2021-11-11 22:43:20 +0530

    add templates file

--- todolist/settings.py
+++ todolist/settings.py
@@ -55,12 +55,13 @@ ROOT_URLCONF = 'todolist.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
@@ -119,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+#link style sheet
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static'),
+]
+
